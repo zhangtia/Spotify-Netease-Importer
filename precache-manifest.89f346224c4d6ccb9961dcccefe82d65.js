164f825c318bfca11b16a0814bcb1f00a9b2a809
zhangtia <zhangtia@umich.edu>
2020-06-01 18:38:18 -0400

    Updates

---
+++ precache-manifest.89f346224c4d6ccb9961dcccefe82d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15923ba1dca9e0f37857db0880c70967",
+    "url": "/Spotify-Netease-Importer/index.html"
+  },
+  {
+    "revision": "6a49eb371d7a388ef4fd",
+    "url": "/Spotify-Netease-Importer/static/css/main.dab061fb.chunk.css"
+  },
+  {
+    "revision": "d0a36c318f2dcfe038e0",
+    "url": "/Spotify-Netease-Importer/static/js/2.fbaf7bdb.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Spotify-Netease-Importer/static/js/2.fbaf7bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a49eb371d7a388ef4fd",
+    "url": "/Spotify-Netease-Importer/static/js/main.6c3e75ea.chunk.js"
+  },
+  {
+    "revision": "49120787c341f031859f",
+    "url": "/Spotify-Netease-Importer/static/js/runtime-main.a692fcf5.js"
+  }
+]);
